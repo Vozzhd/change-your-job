258f1211b3a69cb4eb744339ecda2c8532ad148a
Vozzhd <Vozzhd@yandex.ru>
2024-09-28 19:56:15 +0300

    Добавил в DI SharedPreferencies Добавил константу SHARED_PREFS для удобства доступа

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,14 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataModule = module {
+
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences("ChangeYourJob SharedPreferences", Context.MODE_PRIVATE)
+    }
 
 }
