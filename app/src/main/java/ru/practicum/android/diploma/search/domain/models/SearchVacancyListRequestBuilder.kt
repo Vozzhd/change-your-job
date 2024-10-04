5215573678a73a432402342d9175efea7b156919
Vozzhd <Vozzhd@yandex.ru>
2024-10-04 03:13:55 +0300

    Исправил краш приложения, часть замечания по detekt

--- app/src/main/java/ru/practicum/android/diploma/search/domain/models/SearchVacancyListRequestBuilder.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/models/SearchVacancyListRequestBuilder.kt
@@ -6,16 +6,16 @@ object SearchVacancyListRequestBuilder {
 
     private val expression = StringBuilder(text + perPage)
 
-    fun setText(string: String) {
-        text = "?text=${string}"
+    fun setTextForSearch(string: String) {
+        text = "?text=$string"
     }
 
     fun setArea(areaId: String) {
-        expression.append("&area=${areaId}")
+        expression.append("&area=$areaId")
     }
 
     fun setIndustry(industryId: String) {
-        expression.append("&industry=${industryId}")
+        expression.append("&industry=$industryId")
     }
 
     fun setIfSalarySpecified(salarySpecified: Boolean) {
@@ -25,26 +25,26 @@ object SearchVacancyListRequestBuilder {
     }
 
     fun setSalary(salary: String) {
-        expression.append("&salary=${salary}")
+        expression.append("&salary=$salary")
     }
 
     fun setPage(page: String) {
-        expression.append("&page=${page}")
+        expression.append("&page=$page")
     }
 
     fun setHost(countryId: String) {
-        val host: String = when (countryId) {
-            "113" -> "hh.ru" //Россия
-            "5" -> "hh.ru" //Украина
-            "40" -> "hh.kz" //Казахстан
-            "9" -> "hh1.az" // Азербайджан
-            "16" -> "rabota.by"//Беларусь
-            "28" -> "headhunter.ge"//Грузия
-            "48" -> "headhunter.kg"//Кыргызстан
-            "97" -> "hh.uz" //Узбекистан
+        val host: String = when (countryId.toInt()) {
+            113 -> "hh.ru" //Россия
+            5 -> "hh.ru" //Украина
+            40 -> "hh.kz" //Казахстан
+            9 -> "hh1.az" // Азербайджан
+            16 -> "rabota.by"//Беларусь
+            28 -> "headhunter.ge"//Грузия
+            48 -> "headhunter.kg"//Кыргызстан
+            97 -> "hh.uz" //Узбекистан
             else -> "hh.ru"
         }
-        expression.append("&host==${host}")
+        expression.append("&host==$host")
     }
 
     fun getExpression(): String {
