e9d4bb2ba030f8be289181276054bac4993d81eb
ber9z <berez.a@bk.ru>
2024-10-22 19:46:52 +0300

    детект

--- app/src/main/java/ru/practicum/android/diploma/filters/areas/presentation/region/RegionSelectViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/areas/presentation/region/RegionSelectViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_AUTO
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.filters.areas.domain.api.AreaCashInteractor
 import ru.practicum.android.diploma.filters.areas.domain.api.FilterAreaInteractor
